bc49d284e2bedaeaa9725f5cc05f84b9a9da0cdd
Josh <joshua_butler1@student.uml.edu>
2015-11-01 13:55:33 -0500

    Update Daily15.c
    Function issue fixed.
    Global constant defined to spec

--- Daily15.c
+++ Daily15.c
@@ -1,14 +1,10 @@
 /*
 Author: Joshua Butler
 Date: 10/28/2015
-
-Purpose: To calculate the force of gravity exerted between two bodies 
+Purpose: To calculate the force of gravity exerted between two bodies
 using Newtons Law of Universal Gravitation. Takes input in gcs.
-
 Gravitational force in dynes of moon on earth: 1.98 x 10^ 25
-
 <Time Spent>: 5 hours.
-
 */
 
 #include <stdio.h>
@@ -18,9 +14,7 @@ Gravitational force in dynes of moon on earth: 1.98 x 10^ 25
 enum boolean { FALSE, TRUE };
 typedef enum boolean Boolean;
 
-
-#define BIG_G_GCS  6.673*pow(10, -8) // Universal Gravitational Constant.
-#define BIG_G_KMS	6.673*pow(10,-11) //
+static const double BIG_G = 6.673;
 
 // Precondition: None
 // Postcondition: Inputs are taken in proper units.
@@ -28,7 +22,7 @@ double input();
 
 // Precondition: Proper inputs have been made
 // Postcondition: Outputs the attractive force between units
-double calculate_attractive_force();
+double calculate_attractive_force(double distance, double mass1, double mass2, double grav_constant);
 
 void clear_key();
 Boolean u_continue();
@@ -38,7 +32,7 @@ double unit_choice();
 int main(int argc, char* argv[])
 {
 	double output, distance, mass1, mass2, force_of_bodies, grav_constant;
-	
+
 	printf("The number is scientific notation \n");
 	printf("The format is input1 * 10 ^ input2.\n");
 	printf("For example, for 5.73 * 10 ^-3, input1 is 5.73, input2 is -3.\n\n");
@@ -49,48 +43,42 @@ int main(int argc, char* argv[])
 	do{
 
 		grav_constant = unit_choice();
-		printf("%g \n", grav_constant );
+		printf("%g \n", grav_constant);
 
 
 		do{
-			
+
 			printf("A distance, in CENTIMETER(S): ");
 			distance = input();
 			printf("You entered: %g CENTIMETER(S).\nWhich: is %g KILOMETER(S).\n",
 				distance, distance * pow(10, -5));
-			} while (u_correct() != 1);
+		} while (u_correct() != 1);
 
 		do{
 			printf("A mass for body 1, in GRAM(S) : ");
 			mass1 = input();
-			printf("You entered: %g GRAM(S)\nWhich is: %g KILOGRAMS\n", mass1, mass1 * pow(10,-3));
-		}while (u_correct() != 1);
+			printf("You entered: %g GRAM(S)\nWhich is: %g KILOGRAMS\n", mass1, mass1 * pow(10, -3));
+		} while (u_correct() != 1);
 
 		do{
 			printf("A mass for body 2, in KILOGRAM(S): ");
 			mass2 = input();
-			printf("You entered: %g GRAM(S)\nWhich is %g KILOGRAMS\n", mass2, mass2* pow(10,-3));
+			printf("You entered: %g GRAM(S)\nWhich is %g KILOGRAMS\n", mass2, mass2* pow(10, -3));
 		} while (u_correct() != 1);
 
-		
-
-		force_of_bodies = ((grav_constant * mass1 * mass2) / pow(distance, 2));  // Attempts to calculate force of bodies this works. units are in units.
-																		// Works (originally set up program to input Newtons, may be off by one exponent if I missed changing one thing.)
-
-
-		//force_of_bodies = calculate_attractive_force(distance, mass1, mass2); // Attempts to calculate force of bodies using a function, this doesnt work.
-
-		printf("Attractive force: %g UNITS \n", force_of_bodies); 
+		force_of_bodies = calculate_attractive_force( distance, mass1, mass2, grav_constant); //calculate force of bodies using a function
+		printf("Attractive force: %g UNITS \n", force_of_bodies);
 
 	} while (u_continue());
 
-		
+
 	return(0);
-		}
+}
 
 
-double input()
-{	
+double 
+input()
+{
 	double first, second, last, input2;
 
 	printf("Please enter the first number: ");
@@ -103,66 +91,62 @@ double input()
 	input2 = second;
 
 	last = first * pow(10.0, input2);
-	
+
 
 	return last;
-	
+
 
 
 
 }
 
-/* float calculate_attractive_force(distance, mass1, mass2) // this function has yet to work, I may be entering my inputs incorrectly, trying to take inputs from main to here.
+double
+calculate_attractive_force(double distance, double mass1,double mass2,double grav_constant)
 {
-	float m1, dist, m2;
-		double force;
-	dist = distance;
-	m1 = mass1;
-	m2 = mass2;
-	
+double m1, dist, m2, force, grav;
 
-	printf("****M1: %g **** \n", m1); //test
+dist = distance;
+m1 = mass1;
+m2 = mass2;
+grav = grav_constant;
 
-	printf("****M2: %g****\n", m2);
+printf("****M1: %g **** \n", m1);  // test***
+printf("****M2: %g****\n", m2);
+printf("****DISTANCE: %g **** \n", dist);
+force = ((grav * (m1 * m2)) / 1*pow(dist,2));
 
-	printf("****DISTANCE: %g **** \n", dist);
+return force;
+} 
 
 
-
-	//force = ((BIG_G * (m1 * m2)) / dist);
-
-	force = m1;
-
-	return force;
-
-	} */ 
-	
-
-void clear_key(void)
+void
+clear_key(void)
 {
 	char c;
 
 	scanf("%c", &c);
 	while (c != '\n')
 	{
-	scanf("%c", &c);
-}
+		scanf("%c", &c);
+	}
 }
 
-Boolean u_continue(void)
+Boolean 
+u_continue(void)
 {
 	char answer;
 	do
 	{
+
 		printf("Do you wish to continue? (y/n): ");
 		scanf(" %c", &answer);
 		clear_key();
 
 		if (answer != 'y' && answer != 'Y' && answer != 'n'
-		&& answer != 'N')
-			{
+			&& answer != 'N')
+		{
 			printf("You can only enter 'y' or 'n' \n");
-			}
+		}
 	} while (answer != 'y' && answer != 'Y' && answer != 'n'
 		&& answer != 'N');
 
@@ -171,10 +155,11 @@ Boolean u_continue(void)
 		return TRUE;
 
 	}
-return FALSE;
+	return FALSE;
 }
 
-Boolean u_correct(void)
+Boolean 
+u_correct(void)
 {
 	char answer;
 	do
@@ -200,8 +185,10 @@ Boolean u_correct(void)
 	return FALSE;
 }
 
-double unit_choice(void)
-{
+double 
+unit_choice()
+{		
+	
 	char answer;
 	do
 	{
@@ -209,16 +196,16 @@ double unit_choice(void)
 		scanf(" %c", &answer);
 		clear_key();
 
-		if (answer != '1' && answer != '2' )
+		if (answer != '1' && answer != '2')
 		{
 			printf("You can only enter '1' or '2' \n");
 		}
-	} while (answer != '1' && answer != '2' );
+	} while (answer != '1' && answer != '2');
 
 	if (answer == '1')
 	{
-		return BIG_G_GCS;
+		return  BIG_G*pow(10,-8);
 
 	}
-	return BIG_G_KMS;
+	return  BIG_G*pow(10,-11);
 }
